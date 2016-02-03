9b0481097f1c793af2867c6e512a170dea1bb377
Xiao Ling <lingxiao@seas.upenn.edu>
2016-02-03 17:13:21 -0500

    5:13

--- hw3/Tactics.v
+++ hw3/Tactics.v
@@ -1042,7 +1042,8 @@ Theorem combine_split : forall X Y (l : list (X * Y)) l1 l2,
   split l = (l1, l2) ->
   combine l1 l2 = l.
 Proof.
-  intros X Y l. unfold combine, split.
+  intros X Y l l1 l2. induction l.
+
     
     
 
