08cfb36de2f9679f2390bce60c992c4d4cbdc7ed
Xiao Ling <lingxiao@seas.upenn.edu>
2016-02-05 17:28:02 -0500

    5:28

--- hw4/Logic.v
+++ hw4/Logic.v
@@ -773,6 +773,7 @@ Theorem dist_not_exists : forall (X:Type) (P : X -> Prop),
   (forall x, P x) -> ~ (exists x, ~ P x).
 Proof.
   intros X P H1. unfold not. intros [x Hx].
+  apply Hx. apply H1.
 Abort.    
 
 
