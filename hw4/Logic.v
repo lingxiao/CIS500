dab3e3d66a677a0b54a8b0d9feeb622d9b197758
Xiao Ling <lingxiao@seas.upenn.edu>
2016-02-08 15:39:02 -0500

    3:39

--- hw4/Logic.v
+++ hw4/Logic.v
@@ -1551,47 +1551,51 @@ Fixpoint beq_list {A} (beq : A -> A -> bool)
   | _       , _        => false                    
   end.                          
 
-                              
-(*  intros A. split.
-  induction l1 as [|a1 l1'].
-    + destruct l2.
-        - simpl. reflexivity.
-        - simpl. intros CH. inversion CH.
-    + destruct l2 as [|a2 l2'].
-        - simpl. intros CH. inversion CH.
-        - simpl. destruct (beq a1 a2) eqn: Ha1a2.
-            * intros Hl1l2. replace a1 with a2. replace l1' with l2'. reflexivity.
-                {apply 
 
-*)                
+Lemma list_eq : forall (X : Type) (l1 l2 : list X) (a : X),
+ a :: l1 = a :: l2 -> l1 = l2.
+Proof. admit.  Qed.
+                             
+                              
 Lemma beq_list_true_iff :
   forall A (beq : A -> A -> bool),
     (forall a1 a2, beq a1 a2 = true <-> a1 = a2) ->
     forall l1 l2, beq_list beq l1 l2 = true <-> l1 = l2.
-Proof.  
-  intros A beq Heq l1 l2. split.
-  (* forward direction *)
-  * induction l1 as [|a1 l1'].
-      + destruct l2.
-         - simpl. reflexivity.
-         - simpl. intros CH. inversion CH.
-      + destruct l2 as [|a2 l2'].
-         - simpl. intros CH. inversion CH.
-         - simpl. destruct (beq a1 a2) eqn: Has.
-            apply Heq in Has. rewrite Has. admit.  (*todo: finish this proof *) 
-            intros CH. inversion CH.
-    
-   (* reverse direction*)
-   * intros H. induction l1 as [|a1 l1'].
-       + simpl. destruct l2 as [|a2 l2'].
-           - reflexivity.
-           - inversion H.
-       + simpl. destruct l2 as [|a2 l2'].
-           - inversion H.
-           - destruct (beq a1 a2) eqn: Has.
-               apply Heq in Has. rewrite Has in H.  admit.
-Abort.  (* todo: finish this proof !! *)
+Proof.
+  intros A. split.
 
+    + generalize dependent l2. induction l1 as [|a1 l1'].
+        - destruct l2.
+            * reflexivity.
+            * simpl. intros Hc. inversion Hc.
+        - destruct l2 as [|a2 l2'].
+            * simpl. intros Hc. inversion Hc.
+            * simpl. destruct (beq a1 a2) eqn: Ha.
+                intros Hl. apply IHl1' in Hl. rewrite Hl. 
+                apply H in Ha. rewrite Ha. reflexivity.
+
+                intros Hc. inversion Hc.
+
+   + generalize dependent l2. induction l1 as [|a1 l1'].
+        - destruct l2.
+            * reflexivity.
+            * simpl. intros Hc. inversion Hc.
+        - destruct l2 as [|a2 l2'].
+            * simpl. intros Hc. inversion Hc.
+            * simpl. destruct (beq a1 a2) eqn : Ha.
+                intros Hl. apply H in Ha. rewrite Ha in Hl.
+                apply IHl1'. apply list_eq in Hl. apply Hl.
+
+                intros Hl. 
+                
+                
+
+
+
+
+
+
+              
 
 (** **** Exercise: 2 stars, recommended (All_forallb)  *)
 (** Recall the function [forallb], from the exercise
@@ -1756,8 +1760,9 @@ Theorem excluded_middle_irrefutable:  forall (P:Prop),
 Proof.
   intros P. unfold not.
   (* we want to show:  (P \/ (P -> False) -> False) -> False  *)
-  intros H. apply H. 
-  
+  intros H. apply H.
+  right. intros p. apply H. left. apply p.
+Qed.  
   
   
 
