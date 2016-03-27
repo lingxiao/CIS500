ba29c07725dc239c204fb250adf95e2a9752b8af
Xiao Ling <lingxiao@seas.upenn.edu>
2016-03-27 11:15:26 -0400

    1115

--- hw8/Hoare2.v
+++ hw8/Hoare2.v
@@ -233,15 +233,15 @@ These decorations were constructed as follows:
 (** Fill in valid decorations for the following program:
    {{ True }}
   IFB X <= Y THEN
-      {{                         }} ->>
-      {{                         }}
+      {{  True /\  X <= Y        }} ->>
+      {{  Y = X + (Y - X)        }}
     Z ::= Y - X
-      {{                         }}
+      {{  Y = X + Z              }}
   ELSE
-      {{                         }} ->>
-      {{                         }}
+      {{ True /\ ~ (X <= Y)      }} ->>
+      {{ X + Z = X + Z           }}
     Y ::= X + Z
-      {{                         }}
+      {{ Y = X + Z               }}
   FI
     {{ Y = X + Z }}
 *)
